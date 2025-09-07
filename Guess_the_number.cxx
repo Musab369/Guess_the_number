c466c5767fd9aae3ac2eaf41362e20a7c79805d8
Mussab_Abdullah <saifulmussab@gmail.com>
2025-09-07 09:55:34 +0530

    Add files via upload

---
+++ Guess_the_number.cxx
@@ -0,0 +1,39 @@
+
+//  My First Game In C language ?
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    // Seed the random number generator with current time
+    srand(time(0));
+
+    // Generate and print a random number between 1 and 100
+    int randomnumber = (rand() % 100) + 1;
+    //printf("Random number between 1 and 100: %d\n", randomNumber);
+    int no_of_guesses = 0;
+    int guessed;
+    
+    do{
+        printf("Guess the number\t");
+        scanf("%d", &guessed);
+        if ( guessed > randomnumber){
+            printf("Lower case please!\n");
+        }
+            else if ( guessed < randomnumber){
+                printf("Higher case please!\n");
+            }
+            else{
+                printf("Congrats!!!\n");
+            }
+            no_of_guesses++;
+    }while ( guessed != randomnumber);
+    
+    printf("You guessed the number in %d guesses", no_of_guesses);
+    
+    
+    
+
+    return 0;
+}
